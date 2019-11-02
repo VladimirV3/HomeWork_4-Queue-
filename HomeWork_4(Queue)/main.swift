e7e50c4e85d8bdca47952cc65cb1a4bb862bb071
VladimirV3 <virushere@icloud.com>
2019-11-02 17:32:58 +0300

    done

--- HomeWork_4(Queue)/main.swift
+++ HomeWork_4(Queue)/main.swift
@@ -8,5 +8,63 @@
 
 import Foundation
 
-print("Hello, World!")
+
+protocol QueueProtocol {
+    
+    associatedtype S
+    
+    var count: Int { get }
+    
+    mutating func pop() -> S?
+    
+    mutating func push(el: S)
+
+}
+
+struct Queue <T>: QueueProtocol {
+    
+    private var storage = [T]()
+    
+    typealias S = T
+    
+    var count: Int {
+        get {
+            return storage.count
+        }
+    }
+    
+    mutating func pop() -> S? {
+        
+        if (storage.isEmpty) {
+            return nil
+        } else {
+            return storage.remove(at: 0)
+        }
+    }
+    mutating func push(el: T) {
+        
+        storage.append(el)
+    }
+}
+
+var queueOne = Queue<String>()
+
+queueOne.push(el: "One")
+queueOne.push(el: "Two")
+queueOne.push(el: "Three")
+
+print(queueOne.count)
+
+print(queueOne.pop() ?? "No elements")
+print(queueOne.pop() ?? "No elements")
+print(queueOne.pop() ?? "No elements")
+print(queueOne.pop() ?? "No elements")
+
+
+
+
+
+
+
+
 
